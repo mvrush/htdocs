2c3013720eb4cdcc896eb3430af17a7d9bdef9e1
mvrush <mvrush@gmail.com>
2021-05-05 14:01:52 -0600

    first commit

---
+++ phpmotors/snippets/header.php
@@ -0,0 +1,6 @@
+<header class="clearfix">
+        <div class="headercontent">
+        <a href="index.html"><img src="images/site/logo.png" alt="PHP Motors logo"></a>
+        <p>My Account</p>
+        </div>
+        </header>
---
+++ phpmotors/snippets/nav.php
@@ -0,0 +1,12 @@
+<nav class="clearfix">
+        <div class="mainnavigation">
+            <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Classic</a></li>
+            <li><a href="#">Sports</a></li>
+            <li><a href="#">SUV</a></li>
+            <li><a href="#">Trucks</a></li>
+            <li><a href="#">Used</a></li>
+            </ul>
+        </div>
+        </nav>
