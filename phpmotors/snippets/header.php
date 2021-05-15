29d7e9f78695c378be9d80f6d626c1531d76eeab
mvrush <mvrush@gmail.com>
2021-05-14 21:30:35 -0600

    Enhancement 3 Complete

--- phpmotors/snippets/header.php
+++ phpmotors/snippets/header.php
@@ -1,4 +1,4 @@
         <div class="headercontent">
-        <a href="index.html"><img src="/phpmotors/images/site/logo.png" alt="PHP Motors logo"></a>
+        <a href="/phpmotors"><img src="/phpmotors/images/site/logo.png" alt="PHP Motors logo"></a>
         <a href="/phpmotors/accounts/index.php?action=login"><p>My Account</p></a>
         </div>
