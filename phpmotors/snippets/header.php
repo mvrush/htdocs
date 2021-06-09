8a904eb62c71633212370087beee57e405430ce3
mvrush <mvrush@gmail.com>
2021-06-08 21:34:04 -0600

    fix header link styling in css

--- phpmotors/snippets/header.php
+++ phpmotors/snippets/header.php
@@ -1,7 +1,7 @@
         <div class="headercontent">
         <a href="/phpmotors"><img src="/phpmotors/images/site/logo.png" alt="PHP Motors logo"></a>
         <?php if(isset($_SESSION['loggedin'])){
-        echo "<a href='/phpmotors/accounts/index.php?action=logout'><p>Logout</p></a> <a href='/phpmotors/accounts/index.php?action=admin'<p>Welcome ".$_SESSION['clientData']['clientFirstname']."</p>";
+        echo "<a href='/phpmotors/accounts/index.php?action=logout'><p>Logout</p></a> <a href='/phpmotors/accounts/index.php?action=admin'><p>Welcome ".$_SESSION['clientData']['clientFirstname']."</p></a>";
         // exit;
         }
         else {
