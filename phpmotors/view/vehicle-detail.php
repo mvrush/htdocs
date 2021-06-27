ab11fb4a1019accb0bcd60ab5d2b2a5820cc057e
mvrush <mvrush@gmail.com>
2021-06-26 22:17:08 -0600

    Enhancement 8 complete

--- phpmotors/view/vehicle-detail.php
+++ phpmotors/view/vehicle-detail.php
@@ -5,7 +5,7 @@
         <meta name="viewport"  content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta name="description" content="PHP Motors is a demonstration of PHP in action">
         <meta name="author" content="Matt Rushton">
-        <title>Content Title | PHP Motors</title>
+        <title><?php {echo "$invInfo[invMake] $invInfo[invModel]";}?> | PHP Motors</title>
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Krona+One&family=Roboto:wght@300;400;500;700;900&family=Zen+Dots&display=swap" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="/phpmotors/css/normalize.css"> 
