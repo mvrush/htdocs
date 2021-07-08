4d4949e97c07ab91b75a970a75f8d9640ba5790b
mvrush <mvrush@gmail.com>
2021-07-07 22:54:15 -0600

    Enhancement 9 Complete

--- phpmotors/snippets/footer.php
+++ phpmotors/snippets/footer.php
@@ -1,4 +1,4 @@
         <hr>
         <p>&copy; PHP Motors, All rights reserved</P>
         <p>All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed</p>
-        <p>Last Updated: 14 May, 2021</p>        
+        <p>Last Updated: 7 July, 2021</p>        
